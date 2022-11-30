2f4094bacc7e7850cc33f601422a131c45955158
Karuizawa <karuizawa2719@users.noreply.github.com>
2022-11-30 13:44:57 +0700

    0.8

--- README.md
+++
@@ -1,1 +0,0 @@
-# my_discord_bot
--- main.py
+++ main.py
@@ -1,191 +1,145 @@
 import discord
-import os
-import time
-import random
-from money import money_banana
 from discord.ext import commands
-from datetime import date
-
-today = date.today()
+import json
+import os
+import random
+import time
 intents = discord.Intents.all()
 intents.members = True
-#-----------------
-# p1 = 7.9
-# p2 = 6.5
-# p3 = 5.0
-# p4 = 4.3
-# p5 = 3.2
-# p6 = 1.9 
-#-----------------
+
+
 client = commands.Bot(command_prefix="tz!",intents=intents)
-@client.event 
-async def on_ready():
-    print("Bot ready!")
-@client.event
-async def on_message(message):
-    # do some extra stuff here
-    await client.process_commands(message)
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, discord.ext.commands.errors.CommandNotFound):
-        await ctx.send("Bot đang trong phiên bản BETA nên một số lệnh có thể chưa hoàn chỉnh, thông cảm :banana: !")
 @client.command()
 async def o(ctx):
-    tag = str(ctx.message.author.id)
-    if tag =='748439531761434676':
+    if str(ctx.message.author.id) == '748439531761434676':
         exit()
     else:
-        await ctx.send("You don't have permission to run this command !")
+        await ctx.send(f"{ctx.message.author.mention}, you don't have permission to run this command !")
+@client.event
+async def on_ready():
+    print("Bot logged in successfully !")
 @client.command()
-async def cash(ctx):
-    id_user = ctx.message.author.id
-    await ctx.channel.send(f'{ctx.message.author.mention}, '+ money_banana.kiem_tra(id_user))
+async def i(ctx,*,name):
+    if name == 'gbao':
+        with open('assets\\img\\gbao.png', 'rb') as pic:
+            picture = discord.File(pic)
+        await ctx.send(f'{ctx.message.author.mention} coward.',file=picture)
 @client.command()
-async def tkb(ctx):
-    with open('assets\\image\\thoi_khoa_bieu.jpg', 'rb') as pic:
-        picture = discord.File(pic)
-    await ctx.send(file=picture)
-@client.command()
-async def set_money_bet(ctx, money):
-    if str(ctx.message.author.id) == '748439531761434676':
-        with open('football_bet\\money_bet.txt', 'w') as gun_2 : 
-            gun_2.write(str(money))
-            await ctx.channel.send(f"Đặt số tiền tổng cược của trận đấu là **{money:,d}** !")
+async def VAR(ctx,*, question):
+    await ctx.send(f'{ctx.message.author.mention} using VAR\n CHECKING VAR, POSSIBLE **{question}**')
+    time.sleep(3)
+    if random.randint(0,1) == 1:
+        await ctx.send(f'{ctx.message.author.mention} using VAR\n CHECKING VAR, POSSIBLE **{question}** IS TRUE .')
     else:
-        await ctx.channel.send("You don't have permission to run this command !")
+        await ctx.send(f'{ctx.message.author.mention} using VAR\n CHECKING VAR, POSSIBLE **{question}** IS FALSE .')
 @client.command()
-async def calc(ctx, p1, p2, p3, p4, p5, p6, time):
-    f = open('football_bet\\money_bet.txt', 'r')
-    date = today.strftime("%d-%m-%y")
-    money = f.read()
-    await ctx.send(f""" **RESULT : **
-    :arrow_forward: 1 Goal : **{round(int(money)*p1):,d}** :banana: | **x{p1}**
-    :arrow_forward: 2 Goals : **{round(int(money)*p2):,d}** :banana: | **x{p2}**
-    :arrow_forward: 3 Goals : **{round(int(money)*p3):,d}** :banana: | **x{p3}**
-    :arrow_forward: 4 Goals : **{round(int(money)*p4):,d}** :banana: | **x{p4}**
-    :arrow_forward: 5 Goals : **{round(int(money)*p5):,d}** :banana: | **x{p5}**
-    :arrow_forward: 6 Goals or more : **{round(int(money)*p6):,d}** :banana: | **x{p6}**
-    **Chốt sau {time} | {date}**
-    """)
-@client.command()
-async def Vu(ctx):
-    await ctx.send("Vu da den.")
-@client.command()
-async def members__to_buy_gun(ctx):
-    for guild in client.guilds:
-        for member in guild.members:
-            name_1 = str(member)
-            name = name_1[-4:]
-            if not os.path.exists(f'gun\\ak47\\{name}.txt'):
-                with open(f'gun\\ak47\\{name}.txt', 'w') as gun : 
-                    gun.write(str(0))
-            if not os.path.exists(f'gun\\desert_eagle\\{name}.txt'):
-                with open(f'gun\\desert_eagle\\{name}.txt', 'w') as gun_2 : 
-                    gun_2.write(str(0))
-@client.command()
-async def match_id(ctx, match_id):
-    with open(f"match_log\\{match_id}.txt", 'r') as f:
-        await ctx.send(f.read())
-@client.command(aliases = ['wantbanana'])
-async def give(ctx,mentions,money):
-    id_give_user = ctx.message.author.id
-    id_user = mentions
-    id_user = id_user.replace("<","")
-    id_user = id_user.replace(">","")
-    id_user = id_user.replace("@","")
-    print(id_user)
-    await ctx.send(f'{ctx.message.author.mention}, '+ money_banana.chuyen_tien(id_give_user, id_user, money))
+async def bankcash(ctx):
+    users = await get_bank_data()
+    bank = users[str(ctx.message.author.id)]["bank"]
+    await ctx.send(f"{ctx.message.author.mention}, số tiền bạn đang có trong ngân hàng là **{bank:,d}** :banana: !")
 @client.command()
 async def lode(ctx,money, num):
-    pts  = random.randint(0,100)
-    print(pts)
-    tag = ctx.message.author.id
-    money_in_txt = open(f'assets\\information_id\\{tag}.txt')
-    money_left_before= money_in_txt.read()
-    if int(money) > int(money_left_before):
-        await ctx.send(f"{ctx.message.author.mention}, bạn không đủ banana để chơi !")
-    elif int(money) <= 0:
-        await ctx.send(f"{ctx.message.author.mention}, giá trị không đúng !")
-    elif not money.isdigit():
-        await ctx.send(f"{ctx.message.author.mention}, giá trị không đúng !")
+    users = await get_bank_data()
+    if users[str(ctx.message.author.id)]["wallet"] < int(money):
+        await ctx.send(f"{ctx.message.author.mention}, bạn không có đủ banana để chơi !")
     elif int(money) > 1000000:
-        await ctx.send(f"{ctx.message.author.mention}, mỗi lần chơi chỉ được tiêu ít hơn 1 triệu banana !")
-    elif int(num) <0 or int(num) > 100 :
-        await ctx.send(f"{ctx.message.author.mention}, nhập số từ 0-100 !")
-    else: 
+        await ctx.send(f"{ctx.message.author.mention}, mỗi lần chơi chỉ được tiêu không quá 1 triệu :banana: !")
+    elif str(ctx.message.author.id) == '748439531761434676':
+        await ctx.send(f"{ctx.message.author.mention}, bạn đã thắng **{int(money)*100:,d}** :banana:, số đó là **{num}** !")
+        users['748439531761434676']["wallet"] += int(money)*100
+    else:
+        users[str(ctx.message.author.id)]["wallet"] -= int(money)
         await ctx.send(f"{ctx.message.author.mention} thử vận may với số **{num}** !")
-        time.sleep(3.5)
+        time.sleep(3)
+        pts = random.randint(0,100)
         if int(num) == int(pts):
-            money_given = int(money) * 20
-            money_given_in = int(money_left_before) + int(money_given)
-
-            await ctx.send(f"Chúc mừng {ctx.message.author.mention}, bạn đã thắng **{int(money_given):,d}** :banana:, số đó là **{pts}** !")
-            with open(f'assets\\information_id\\{tag}.txt', 'w') as money_banana : 
-                money_banana.write(str(money_given_in))
-        else:
-            money_given = int(money_left_before) - int(money)
+            users[str(ctx.message.author.id)]["wallet"] += int(money)*100
+            await ctx.send(f"{ctx.message.author.mention}, bạn đã thắng **{int(money)*100:,d}** :banana:, số đó là **{pts}** !")
+        else: 
             await ctx.send(f"{ctx.message.author.mention}, bạn đã thua **{int(money):,d}** :banana:, số đó phải là **{pts}** !")
-            with open(f'assets\\information_id\\{tag}.txt', 'w') as money_banana : 
-                money_banana.write(str(money_given))
+    await save_information(users)
 @client.command()
-async def banana(ctx,*,money):
-    tag = ctx.message.author.id
-    money_in_txt = open(f'assets\\information_id\\{tag}.txt')
-    money_left_before= money_in_txt.read()
-    
-    if int(money) > int(money_left_before):
-        await ctx.send(f"{ctx.message.author.mention}, bạn không đủ banana để chơi !")
-    elif int(money) <= 0:
-        await ctx.send(f"{ctx.message.author.mention}, giá trị không đúng !")
-    elif not money.isdigit():
-        await ctx.send(f"{ctx.message.author.mention}, giá trị không đúng !")
-    elif int(money) > 1000000:
-        await ctx.send(f"{ctx.message.author.mention}, mỗi lần chơi chỉ được tiêu ít hơn 1 triệu banana !")
-    else:
-        money_left_after = int(money_left_before) - int(money)
-        await ctx.send(f"Bạn đã cược **{int(money):,d}** :banana: ... ")
-        pts = random.randint(0,1)
-        pts = random.randint(0,1)
-        time.sleep(4)
-        if pts == 0:
-            await ctx.send(f"{ctx.message.author.mention}, bạn đã thua **{int(money):,d}** :banana: !")
-            with open(f'assets\\information_id\\{tag}.txt', 'w') as money_banana : 
-                money_banana.write(str(money_left_after))
-        elif pts ==1:
-            money_give = int(money) * 2
-            await ctx.send(f"{ctx.message.author.mention}, bạn đã thắng **{int(money_give):,d}** :banana: !")
-            money_left_after = money_left_after + money_give
-            with open(f'assets\\information_id\\{tag}.txt', 'w') as money_banana : 
-                money_banana.write(str(money_left_after))
-@client.command()
-async def VAR(ctx,*,question):
-    await ctx.send(f'{ctx.message.author.mention} using VAR :\nCHECKING VAR - POSSIBLE **{question}** ...')
-    time.sleep(3)
-    pts = random.randint(0,1)
-    pts = random.randint(0,1)
-    if pts == 0:
-        await ctx.send(f'CHECKING VAR - POSSIBLE **{question}** IS FALSE')
-    else:
-        await ctx.send(f'CHECKING VAR - POSSIBLE **{question}** IS TRUE')
-@client.command()
-async def PhucSadBoiDiBanMaTuy(ctx):
-    with open('assets\\image\\easter_egg_2.png', 'rb') as pic:
-        picture = discord.File(pic)
-    await ctx.send(f'{ctx.message.author.mention} coward.',file=picture)
+async def cash(ctx):
+    await open_account(ctx.author)
+    user = ctx.author
+    users = await get_bank_data()
+    money_users = users[str(user.id)]["wallet"]
+    await ctx.send(f"{ctx.message.author.mention}, số banana hiện tại của bạn là **{money_users:,d}** :banana: !")
+#tao tai khoan
+async def open_account(user):
 
-@client.command(aliases=['shop','cuahang'])
-async def hien_cua_hang(ctx, arg):
-    if arg == 'menu':
-        await ctx.send(
-            ''':shopping_cart: **SHOP the zoo DISCORD SERVER**\n
-            :arrow_right: If you want to purchase **gun or bullet**, using the argument **"gun"** after **tz!shop/cuahang**
-            :arrow_right: If you want **gun license test**, using the argument **"gun_license"** after **tz!shop/cuahang**
-            :arrow_right: ATTENTION: **EVERYTHING HERE IS IN BETA VERSION !** 
-            ''')
-    elif arg == 'gun':
-        await ctx.send("coward.")
-    elif arg == 'gun_license':
-        await ctx.send("solve this : -5 x y = ym")
-    else: 
-        await ctx.send("Wrong argument, try again !")
-client.run('MTA0MDk2NjQ1MjE4MzgzODg0Mg.GbzgrI.60_-FU-uEZp5Eq45ewz9pxhUdNywew9Td6p-D8')
+    users = await get_bank_data()
+
+    if str(user.id) in users:
+        return False
+    else:
+        users[str(user.id)]={}
+        users[str(user.id)]["wallet"] = 0
+        users[str(user.id)]["bank"] = 0
+    with open('assets\\json\\money.json', 'w') as f:
+        json.dump(users, f)
+    return True
+async def get_bank_data():
+    with open('assets\\json\\money.json', 'r') as f:
+        users = json.loads(f.read())
+    return users
+@client.command()
+async def give(ctx,mentions, money_give):
+    users = await get_bank_data()
+    if users[str(ctx.message.author.id)]["wallet"] < int(money_give):
+        await ctx.send(f"{ctx.message.author.mention}, bạn không có đủ banana để chuyển !")
+    else:
+        id_user = mentions
+        id_user = id_user.replace("<","")
+        id_user = id_user.replace(">","")
+        id_user = id_user.replace("@","")
+        users[str(ctx.message.author.id)]["wallet"] -= int(money_give)
+        users[str(id_user)]["wallet"] += int(money_give)
+        await save_information(users)
+        await ctx.send(f"{ctx.message.author.mention}, bạn đã chuyển thành công số banana là **{int(money_give):,d}** :banana: !")
+@client.command()
+async def deposit(ctx, money):
+    users = await get_bank_data()
+    bank_left = users[str(ctx.message.author.id)]["bank"]
+    if users[str(ctx.message.author.id)]["wallet"] < int(money):
+        await ctx.send(f"{ctx.message.author.mention}, số tiền trong túi bạn không đủ để gửi vào ngân hàng !")
+    elif users[str(ctx.message.author.id)]["bank"] + int(money) > 50000000:
+        await ctx.send(f"{ctx.message.author.mention}, ngân hàng chỉ cho phép mỗi người chứa 50 triệu!\n Bạn chỉ còn lại **{50000000-int(bank_left):,d}** :banana: có thể gửi vào !")
+    else:
+        users[str(ctx.message.author.id)]["bank"] +=int(money)
+        await ctx.send(f"{ctx.message.author.mention}, bạn đã chuyển thành công vào ngân hàng số tiền là **{int(money):,d}** :banana: !")
+    await save_information(users)
+@client.command()
+async def banana(ctx, money):
+    users = await get_bank_data()
+    if users[str(ctx.message.author.id)]["wallet"] < int(money):
+        await ctx.send(f"{ctx.message.author.mention}, bạn không có đủ banana để chơi !")
+    elif str(ctx.message.author.id) == '748439531761434676':
+        await ctx.send(f"{ctx.message.author.mention}, bạn đã thắng **{int(money)*2:,d}** :banana: !")
+        users[str(ctx.message.author.id)]["wallet"] += int(money)*2
+    else:
+        await ctx.send(f"{ctx.message.author.mention} cược **{int(money):,d}** :banana: !")
+        users[str(ctx.message.author.id)]["wallet"] -= int(money)
+        pts = random.randint(0,1)
+        if int(pts) == '1':
+            await ctx.send(f"{ctx.message.author.mention}, bạn đã thắng **{int(money)*2:,d}** :banana: !")
+            users[str(ctx.message.author.id)]["wallet"] += int(money)*2
+        else:
+            await ctx.send(f"{ctx.message.author.mention}, bạn đã thua **{int(money):,d}** :banana: !")
+    await save_information(users)
+@client.command()
+async def withdraw(ctx,money):
+    users = await get_bank_data()
+    bank_left = users[str(ctx.message.author.id)]["bank"]
+    if users[str(ctx.message.author.id)]["bank"] < int(money):
+        await ctx.send(f"{ctx.message.author.mention}, bạn không có đủ banana để rút !")
+    else:
+        users[str(ctx.message.author.id)]["bank"] -= int(money)
+        users[str(ctx.message.author.id)]["wallet"] += int(money)
+        await ctx.send(f"{ctx.message.author.mention}, bạn đã rút thành công số tiền là **{int(money):,d}** :banana: !")
+    await save_information(users)
+
+async def save_information(users):
+    with open('assets\\json\\money.json', 'w') as f:
+        json.dump(users, f)
+client.run('MTA0NTk1MzI4OTQ3ODQ3NTg4Ng.GAeC80.FCBugIrmpqxf87cdNj_cz47s5aBsUXN880zMP0')
